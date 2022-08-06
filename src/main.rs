675a935a8a8b5133510e6a0c86a78671b9eeaefd
Azu0925 <yamaneko564783@gmail.com>
2022-08-06 17:32:23 +0900

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
+use clap::{App, Arg};
 use serde_json::json;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let webhook = "";
+fn main() {
+    let app = App::new("send_my_address")
+        .version("0.1.0")
+        .about("send my address")
+        .arg(
+            Arg::new("webhook")
+                .short('w')
+                .long("webhook")
+                .takes_value(true)
+                .required(true),
+        );
 
+    match app.try_get_matches() {
+        Ok(m) => {
+            let webhook = m.value_of("webhook").unwrap();
+            send(webhook);
+        }
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
+}
+
+#[tokio::main]
+async fn send(webhook: &str) -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
     let res = client
         .post(webhook)

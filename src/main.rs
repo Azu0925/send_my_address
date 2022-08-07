941ea3af479c7a4762cb8853b06c5f4a20042a60
Azu0925 <yamaneko564783@gmail.com>
2022-08-07 19:45:36 +0900

    とりまdone

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use clap::{App, Arg};
+use network_interface::NetworkInterface;
+use network_interface::NetworkInterfaceConfig;
+use serde::Serialize;
 use serde_json::json;
 
 fn main() {
+    let network_interfaces = NetworkInterface::show().unwrap();
+    let mut results: String = String::from("");
+    for itf in network_interfaces.iter() {
+        let address: &network_interface::Addr = &itf.addr.unwrap();
+        let ip = match address {
+            &network_interface::Addr::V4(v4_if_addr) => v4_if_addr.ip.to_string(),
+            &network_interface::Addr::V6(v6_if_addr) => v6_if_addr.ip.to_string(),
+        };
+        let result = String::from("`name:") + &itf.name + ", address: " + &ip + "`\n";
+        results.push_str(String::from(result).as_str());
+    }
+
     let app = App::new("send_my_address")
         .version("0.1.0")
         .about("send my address")
@@ -16,7 +31,8 @@ fn main() {
     match app.try_get_matches() {
         Ok(m) => {
             let webhook = m.value_of("webhook").unwrap();
-            send(webhook);
+            println!("{}", results);
+            send(webhook, results);
         }
         Err(e) => {
             println!("{}", e);
@@ -25,11 +41,11 @@ fn main() {
 }
 
 #[tokio::main]
-async fn send(webhook: &str) -> Result<(), Box<dyn std::error::Error>> {
+async fn send(webhook: &str, results: String) -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
     let res = client
         .post(webhook)
-        .json(&json!({"content": "aaa"}))
+        .json(&json!({ "content": &results }))
         .send()
         .await?
         .text()
@@ -37,4 +53,9 @@ async fn send(webhook: &str) -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Body: {:?}", res);
     Ok(())
+}
+
+#[derive(Serialize)]
+struct Content {
+    content: std::vec::Vec<String>,
 }

ae7cf76975541af3219728d661a44dec28bcdf1e
Azu0925 <yamaneko564783@gmail.com>
2022-08-08 00:19:21 +0900

    ver0.2.0

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,11 @@ use network_interface::NetworkInterface;
 use network_interface::NetworkInterfaceConfig;
 use serde::Serialize;
 use serde_json::json;
+use std::{thread, time};
 
 fn main() {
-    let network_interfaces = NetworkInterface::show().unwrap();
-    let mut results: String = String::from("");
-    for itf in network_interfaces.iter() {
-        let address: &network_interface::Addr = &itf.addr.unwrap();
-        let ip = match address {
-            &network_interface::Addr::V4(v4_if_addr) => v4_if_addr.ip.to_string(),
-            &network_interface::Addr::V6(v6_if_addr) => v6_if_addr.ip.to_string(),
-        };
-        let result = String::from("`name:") + &itf.name + ", address: " + &ip + "`\n";
-        results.push_str(String::from(result).as_str());
-    }
-
     let app = App::new("send_my_address")
-        .version("0.1.0")
+        .version("0.2.0")
         .about("send my address")
         .arg(
             Arg::new("webhook")
@@ -31,8 +20,7 @@ fn main() {
     match app.try_get_matches() {
         Ok(m) => {
             let webhook = m.value_of("webhook").unwrap();
-            println!("{}", results);
-            send(webhook, results);
+            try_request(webhook, 0);
         }
         Err(e) => {
             println!("{}", e);
@@ -40,8 +28,44 @@ fn main() {
     }
 }
 
+fn try_request(webhook: &str, count: i32) {
+    match send(webhook) {
+        Ok(res) => {
+            println!("Ok: {:?}", res);
+        }
+        Err(err) if count == 10 => {
+            println!("Sorry, can not connected..\n Error: {}", err);
+        }
+        Err(err) => {
+            let ten_secs = time::Duration::from_secs(10);
+            thread::sleep(ten_secs);
+            let next = count + 1;
+            println!("{} try failed, Error message: {}", &next, err);
+            try_request(webhook, next);
+        }
+    }
+}
+
+fn get_network() -> String {
+    let network_interfaces = NetworkInterface::show().unwrap();
+    let mut results: String = String::from("");
+    for itf in network_interfaces.iter() {
+        let address: &network_interface::Addr = &itf.addr.unwrap();
+        let ip = match address {
+            &network_interface::Addr::V4(v4_if_addr) => v4_if_addr.ip.to_string(),
+            &network_interface::Addr::V6(v6_if_addr) => v6_if_addr.ip.to_string(),
+        };
+        let result = String::from("`name:") + &itf.name + ", address: " + &ip + "`\n";
+        results.push_str(String::from(result).as_str());
+    }
+    println!("{}", results);
+
+    results
+}
+
 #[tokio::main]
-async fn send(webhook: &str, results: String) -> Result<(), Box<dyn std::error::Error>> {
+async fn send(webhook: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let results = get_network();
     let client = reqwest::Client::new();
     let res = client
         .post(webhook)
